733cc1edaebb1ffe185a193fdcb20425eceac5df
sharan <139887231+sharann16@users.noreply.github.com>
2024-07-28 15:26:22 +0530

    Delete JavaScript_Programming_Exercises directory

--- JavaScript_Programming_Exercises/05_JS_Arguments/Ch5_pgm_03.js
+++
@@ -1,9 +0,0 @@
-var showMessage;
-
-showMessage = function (message) {
-	console.log("The message is: " + message + ". This is an additional text.");
-};
-
-showMessage("It's full of stars!");
-showMessage("Hello, world!");
-showMessage("Goodbye, everyone!");
